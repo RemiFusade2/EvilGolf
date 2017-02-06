e16385128de8ac16851c09d57b732d7b6466eaea
Remi <remi.fusade@gmail.com>
2017-02-06 23:58:56 +0100

    Project creation
    Signed-off-by: Remi <remi.fusade@gmail.com>

---
+++ EvilGolf_Unity/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour {
+
+	public GameObject ball;
+
+	public float zoom;
+	public float minZoom;
+	public float maxZoom;
+
+	private float lastMouseXPosition;
+	public float mouseSensitivity;
+
+	public Vector2 ballPositionInScreen;
+
+	public float yaw; // mouse Y
+	public float yawMin;
+	public float yawMax;
+
+	public float pitch; // mouse X
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		lastMouseXPosition = Input.mousePosition.x;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		// rotation pitch
+		float mouseXDelta = Input.mousePosition.x - lastMouseXPosition;
+		pitch += (mouseXDelta * mouseSensitivity);
+		lastMouseXPosition = Input.mousePosition.x;
+
+		// rotation yaw
+		float yawRatio = 1-(Input.mousePosition.y / Screen.height);
+		yaw = (yawMin + yawRatio * (yawMax - yawMin));
+
+		Rotate (pitch, yaw);
+
+		// zoom
+		zoom -= Input.mouseScrollDelta.y;
+		zoom = (zoom < minZoom) ? minZoom : ((zoom > maxZoom) ? maxZoom : zoom);
+		Translate (zoom);
+
+	}
+
+	public void Rotate(float inputPitch, float inputYaw)
+	{
+		this.transform.rotation = Quaternion.identity;
+		this.transform.RotateAround (ball.transform.position, this.transform.up, inputPitch);
+		this.transform.RotateAround (ball.transform.position, this.transform.right, inputYaw);
+	}
+
+	public void Translate(float distance)
+	{
+		Vector3 cameraBallVec = (this.transform.forward + ballPositionInScreen.x * this.transform.right + ballPositionInScreen.y * this.transform.up).normalized;
+		this.transform.position = ball.transform.position - distance * cameraBallVec;
+	}
+}
---
+++ EvilGolf_Unity/Assets/Scripts/ControlScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlScript : MonoBehaviour {
+
+	public Rigidbody ball;
+	public Transform camera;
+
+	public float putStrength;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			PushBall (putStrength);
+		}
+	}
+
+	public void PushBall(float force)
+	{
+		ball.AddForce ( (camera.forward + camera.up) * force, ForceMode.Impulse);
+	}
+}
